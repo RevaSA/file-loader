a1c0a834112cb1a00865e0b1948e6039cd9055d2
Stanislav Reva <RevaSA95@gmail.com>
2021-02-14 17:40:57 +0300

    Add bytesToSize function

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -10,3 +10,10 @@ export function getImageFromFile(file) {
         reader.readAsDataURL(file)
     })
 }
+
+export function bytesToSize(bytes = 0) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    const i = Math.floor(Math.log(bytes) / Math.log(1024))
+
+    return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i]
+}
